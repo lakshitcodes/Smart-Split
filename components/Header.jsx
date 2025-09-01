7d66a134afe7f08b6eaf6eda8962f8aba0fe4402
Lakshit Jain <jainlakshit849@gmail.com>
2025-09-01 22:43:06 +0530

    Convex Integrated with Clerk

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,27 @@
-import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
-import { SignedIn, SignedOut } from '@clerk/nextjs'
-import React from 'react'
+"use client";
+
+import { useStoreUser } from "@/hooks/use-store-user";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import React from "react";
+import { BarLoader } from "react-spinners";
 
 const Header = () => {
+  const { isLoading } = useStoreUser();
   return (
-    <div>
+    <header className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
+      <nav>
         <SignedOut>
-            <SignInButton/>
-            <SignUpButton/>
+          <SignInButton />
+          <SignUpButton />
         </SignedOut>
         <SignedIn>
-            <UserButton/>
+          <UserButton />
         </SignedIn>
-    </div>
-  )
-}
+      </nav>
+      {isLoading && <BarLoader width={"100%"} color="#36d7b7" />}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
